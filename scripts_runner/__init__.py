47da40fa58d9c0ef532da32f5ccb298d63b954bd
Deniau Antonin <antonin.deniau@protonmail.com>
2018-02-25 15:55:12 +0100

    readme

--- scripts_runner/__init__.py
+++ scripts_runner/__init__.py
@@ -4,18 +4,18 @@ from server import Server
 srv = Server("stopped", can_cancel=True)
 
 def server_status(bv, fobj):
-	print "[script_server] Server status: %s" % srv.progress
+	print "[script_runner] Server status: %s" % srv.progress
 
 def start_server(bv, fobj):
 	if srv.progress == "running":
-		print "[script_server] Server already started."
+		print "[script_runner] Server already started."
 		return
 	
-	print "[script_server] Starting the server."
+	print "[script_runner] Starting the server."
 	srv.bv = bv
 	srv.start()
 	srv.progress = "running"
-	print "[script_server] Server started."
+	print "[script_runner] Server started."
 
-PluginCommand.register_for_function("[script_server] Server status", "Get the server status", server_status)
-PluginCommand.register_for_function("[script_server] Start server", "Start/Restart the server", start_server)
+PluginCommand.register_for_function("[script_runner] Server status", "Get the server status", server_status)
+PluginCommand.register_for_function("[script_runner] Start server", "Start/Restart the server", start_server)
