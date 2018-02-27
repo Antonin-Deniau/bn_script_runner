d81d6327ae5e177105479efc349d44b9194b446f
Antonin Deniau <Antonin-Deniau@users.noreply.github.com>
2018-02-27 15:40:54 +0100

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env runner
-from binaryninja import HighlightStandardColor
-
-block = bv.get_basic_blocks_at(0x8670)[0]
-block.highlight = HighlightStandardColor.BlueHighlightColor
-print(bv)
